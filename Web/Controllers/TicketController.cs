692b0f9f9a22fd1e515f64451e9395e99925dd1a
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-09-13 11:02:46 +0300

    add tickets and a test controller

---
+++ Web/Controllers/TicketController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TicketController : ControllerBase
+    {
+        private readonly TigetsContext _tigetsContext;
+        private readonly IMapper _mapper;
+        public TicketController(
+            TigetsContext tigetsContext,
+            IMapper mapper
+        )
+        {
+            _tigetsContext = tigetsContext;
+            _mapper = mapper;
+        }
+
+        [HttpPost("ticket")]
+        public async Task<Ticket> Post([FromBody] TicketPostModel ticketPostModel)
+        {
+            var ticket = _mapper.Map<Ticket>(ticketPostModel);
+            ticket.Id = "1";
+            _tigetsContext.Tickets.Add(ticket);
+            _tigetsContext.SaveChanges();
+            return ticket;
+        }
+    }
+}
