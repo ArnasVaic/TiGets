c9a554c10603ef5c0871e02e71fd0654c8cff6eb
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-09-18 16:15:11 +0300

    ticket view

---
+++ Tigets.Core/Models/TicketViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tigets.Core.Models
+{
+    public class TicketViewModel
+    {
+        public TicketState State { get; set; }
+
+        public DateTime ValidFrom { get; set; }
+
+        public DateTime ValidTo { get; set; }
+
+        public string EventName { get; set; }
+
+        public string Address { get; set; }
+
+        public decimal Cost { get; set; }
+    }
+}
