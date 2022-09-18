f9d4526053572e453409508d78feb9c20b17f036
Naglis Šuliokas <naglis1134@gmail.com>
2022-09-18 21:48:42 +0300

    add bare login page with functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ProfilePage from "./pages/profilePage/ProfilePage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from './pages/loginPage/LoginPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-              <Route path="/" element={<ProfilePage />} />
-             
+        <Route path="/" element={<ProfilePage />} />
+        <Route path="/login/ReturnUrl=:returnUrl" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
