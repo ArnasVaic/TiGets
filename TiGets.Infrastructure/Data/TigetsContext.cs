e17b022f49c571a9b28ba72ba1e6efad7ab82873
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-09-18 17:35:11 +0300

    add initial entities to tigets context

--- TiGets.Infrastructure/Data/TigetsContext.cs
+++ TiGets.Infrastructure/Data/TigetsContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Tigets.Core.Models;
@@ -8,9 +9,10 @@ namespace Tigets.Infrastructure.Data
 {
     public class TigetsContext : DbContext
     {
-        public TigetsContext(DbContextOptions<TigetsContext> options)
+        public TigetsContext(DbContextOptions<TigetsContext> options) 
             : base(options) { }
-
         public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Transfer> Transfers { get; set; }
     }
 }
