4ae8b6b4e46029436ac0f9cea330d72784c9e8de
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-09-19 15:22:34 +0300

    first spec

---
+++ Tigets.Core/Specifications/UserByNameAndPasswordHashSpecification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ardalis.Specification;
+using Tigets.Core.Models;
+
+namespace Tigets.Core.Specifications
+{
+    public class UserByNameAndPasswordHashSpecification : Specification<User>
+    {
+        public UserByNameAndPasswordHashSpecification(string username, string passwordHash)
+        {
+            Query.Where(x => x.Username == username && x.PasswordHash == passwordHash);
+        }
+    }
+}
