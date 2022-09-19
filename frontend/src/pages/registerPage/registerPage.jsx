479bfb724b22b7ae3f2f1d9709111eba3c157fd9
Rugile Vasaityte <rugile.vasaityte@gmail.com>
2022-09-19 15:12:08 +0300

    Updated register page + added crappy cpsw validation

--- frontend/src/pages/registerPage/registerPage.jsx
+++ frontend/src/pages/registerPage/registerPage.jsx
@@ -1,17 +1,87 @@
-
+import { Button, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+//import { postRegister } from "../../services/registerService";
 
 function RegisterPage() {
 
+    const [username, setUsername] = useState('');
+    const[password, setPassword] = useState('');
+    const [cPassword, setCPassword] = useState('');
+    const [showErrorMessage, setShowErrorMessage] = useState('');
+   // const { returnUrl } = useParams();
+
+    function checkValidation(){
+        if (password !== cPassword) 
+             setShowErrorMessage("Passwords must match")
+    }
 
     return (
 
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
+     <div>
+      
+        <div
+            style={{ display: "flex", flexDirection: "column", padding: "10% 40%" }}
+         >
+            <Typography variant="h2" style={{ textAlign: "center" }}>
+                TIGETS
+            </Typography>
 
-          <h1>Register Page</h1>
+            <TextField
+                label="Username"
+                onChange={(event) => {
+                    setUsername(event.target.value);
+                }}
+            />
+
+            <TextField
+                label="Password"
+                type="password"
+                onChange={(event) => {
+                    setPassword(event.target.value);
+                }}
+            />
+
+            <TextField
+                label="Confirm password"
+                type="password"
+                onChange={(event) => {
+                    setCPassword(event.target.value);
+                }}
+            />
+
+            <div>
+                {showErrorMessage}
+            </div>
+
+            <div style={{ display: "flex", flexDirection: "column", padding: "2% 0%" }} >
+
+            <Button
+               variant="contained"
+               
+                onClick={() => {
+                    if (password == cPassword) {
+                        console.log("hurray");
+                     //     dispatch(postRegister(username, password, returnUrl)
+                    }
+                    else {
+                        checkValidation();
+                    }
+               }}
+             >
+                Register
+             </Button>
+
+            </div>
+
+         </div>
 
       </div>
-
-  );
-} 
+    );
+}
 
 export default RegisterPage;
+
+
+
