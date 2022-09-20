5de2618a2cf4cb94fbfe70a7ae1366c6c183739f
Rugile Vasaityte <rugile.vasaityte@gmail.com>
2022-09-21 01:01:01 +0300

    Register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import ProfilePage from "./pages/profilePage/ProfilePage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from './pages/loginPage/LoginPage';
+import RegisterPage from './pages/registerPage/RegisterPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
+     <Routes>
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/" element={<ProfilePage />} />
         <Route path="/login/ReturnUrl=:returnUrl" element={<LoginPage />} />
       </Routes>
