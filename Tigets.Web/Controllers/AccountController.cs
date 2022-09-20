60b346656b91be5a5fdb4daf12d1dd8725bac9ea
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-09-20 12:59:36 +0300

    register endpoint

--- Tigets.Web/Controllers/AccountController.cs
+++ Tigets.Web/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Tigets.Core.Models;
 using Tigets.Core.Repositories;
@@ -11,20 +13,30 @@ namespace Tigets.Web.Controllers
     [Route("api/[controller]")]
     public class AccountController : ControllerBase
     {
-        public AccountController()
+        private readonly UserManager<IdentityUser> _userManager;
+        public AccountController(UserManager<IdentityUser> userManager)
         {
-
+            _userManager = userManager;
         }
 
-        [HttpGet("login")]
-        public async Task<IActionResult> Login([FromQuery] string username, [FromQuery] string passwordHash)
+        [AllowAnonymous]
+        [HttpGet("Login")]
+        public async Task<IActionResult> Login([FromQuery] string username, [FromQuery] string password)
         {
             return Ok();
         }
 
-        [HttpPost("register")]
-        public async Task<IActionResult> Register([FromQuery] string username, [FromQuery] string passwordHash)
+        [AllowAnonymous]
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromQuery] string username, [FromQuery] string password)
         {
+            var user = await _userManager.FindByNameAsync(username);
+            if (user != null)
+                throw new Exception("User with this username already exists");
+
+            user = new IdentityUser(username) { EmailConfirmed = true };
+            await _userManager.CreateAsync(user, password);
+
             return Ok();
         }
     }
