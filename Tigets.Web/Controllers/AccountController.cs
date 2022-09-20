714956d1edfaa0ce0a4c829f2fe776e31315f263
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-09-20 11:06:31 +0300

    redo account controller

--- Tigets.Web/Controllers/AccountController.cs
+++ Tigets.Web/Controllers/AccountController.cs
@@ -11,48 +11,20 @@ namespace Tigets.Web.Controllers
     [Route("api/[controller]")]
     public class AccountController : ControllerBase
     {
-        private readonly IUserRepository _userRepository;
-
-        public AccountController(IUserRepository userRepository)
+        public AccountController()
         {
-            _userRepository = userRepository;
+
         }
 
-        [HttpPost("login")]
-        public async Task<IActionResult> Login(
-            [FromQuery] string username, 
-            [FromQuery] string passwordHash
-        )
+        [HttpGet("login")]
+        public async Task<IActionResult> Login([FromQuery] string username, [FromQuery] string passwordHash)
         {
-            if (username == "admin" && password == "123")
-            {
-                var claims = new List<Claim>();
-                claims.Add(new Claim("username", username));
-                claims.Add(new Claim(ClaimTypes.NameIdentifier, username));
-                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-                var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
-                await HttpContext.SignInAsync(claimsPrincipal);
-                return Ok();
-            }
-            return BadRequest();
+            return Ok();
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromQuery] string username, [FromQuery] string passwordHash)
         {
-            var user = new User
-            {
-                Id = Guid.NewGuid().ToString(),
-                Name = username,
-                Surname = username,
-                Username = username,
-                PasswordHash = passwordHash,
-                Email = "",
-                Balance = 0,
-                PhoneNumber = ""
-            };
-            await _userRepository.AddAsync(user);
-            await _userRepository.SaveChangesAsync();
             return Ok();
         }
     }
