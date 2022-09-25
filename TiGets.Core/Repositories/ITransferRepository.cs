ec2f2533cbc4c9ceee85d957de2d6e236c65290a
MrRobot2002 <just.zubavicius@gmail.com>
2022-09-25 14:41:05 +0300

    Created a TransferRepository :)

---
+++ TiGets.Core/Repositories/ITransferRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tigets.Core.Models;
+using Tigets.Core.Repositories;
+using Tigets.Infrastructure.Data;
+
+namespace Tigets.Core.Repositories
+{
+    public interface ITransferRepository : ITransferRepository<Transfer>
+    {
+
+    }
+}
