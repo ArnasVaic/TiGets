2f9c5e65ce9b981bedff1fb1360e1dd3a21d2c60
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-09-25 10:11:45 +0300

    implement register/login endpoints

--- Tigets.Web/Controllers/AccountController.cs
+++ Tigets.Web/Controllers/AccountController.cs
@@ -4,8 +4,10 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Tigets.Core.Constants;
 using Tigets.Core.Models;
 using Tigets.Core.Repositories;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 namespace Tigets.Web.Controllers
 {
@@ -13,9 +15,14 @@ namespace Tigets.Web.Controllers
     [Route("api/[controller]")]
     public class AccountController : ControllerBase
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
-        public AccountController(UserManager<IdentityUser> userManager)
+        public AccountController(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager
+        )
         {
+            _signInManager = signInManager;
             _userManager = userManager;
         }
 
@@ -23,7 +30,16 @@ namespace Tigets.Web.Controllers
         [HttpGet("Login")]
         public async Task<IActionResult> Login([FromQuery] string username, [FromQuery] string password)
         {
-            return Ok();
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user is null)
+                throw new Exception("User does not exist.");
+
+            var result = await _signInManager.PasswordSignInAsync(user, password, false, false);
+
+            if(result.Succeeded)
+                return Ok();
+            return BadRequest(result.ToString());
         }
 
         [AllowAnonymous]
@@ -35,7 +51,10 @@ namespace Tigets.Web.Controllers
                 throw new Exception("User with this username already exists");
 
             user = new IdentityUser(username) { EmailConfirmed = true };
-            await _userManager.CreateAsync(user, password);
+            var result = await _userManager.CreateAsync(user, password);
+
+            if (!result.Succeeded)
+                return BadRequest(result.Errors);
 
             return Ok();
         }
