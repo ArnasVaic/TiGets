546e032cf39a195eeefef05850d92e8bf1e8fa1e
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-10-01 14:10:58 +0300

    move login/register logic to account service

--- Tigets.Web/Commons/ServiceInjectorExtensions.cs
+++ Tigets.Web/Commons/ServiceInjectorExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Tigets.Core.Models;
 using Tigets.Core.Repositories;
+using Tigets.Core.Services;
 using Tigets.Infrastructure.Repositories;
 
 namespace Tigets.Web.Commons
@@ -9,6 +10,7 @@ namespace Tigets.Web.Commons
         public static void InjectServices(this IServiceCollection services)
         {
             services.AddScoped<ITicketRepository, TicketRepository>();
+            services.AddScoped<IAccountService, AccountService>();
         }
     }
 }
--- Tigets.Web/Controllers/AccountController.cs
+++ Tigets.Web/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Tigets.Core.Models;
 using Tigets.Core.Repositories;
+using Tigets.Core.Services;
 using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 namespace Tigets.Web.Controllers
@@ -15,59 +16,43 @@ namespace Tigets.Web.Controllers
     [Route("api/[controller]")]
     public class AccountController : ControllerBase
     {
-        private readonly SignInManager<User> _signInManager;
-        private readonly UserManager<User> _userManager;
-        private readonly IMapper _mapper;
+        private readonly IAccountService _accountService;
 
-        public AccountController(
-            UserManager<User> userManager,
-            SignInManager<User> signInManager,
-            IMapper mapper
-        )
+        public AccountController(IAccountService accountService)
         {
-            _signInManager = signInManager;
-            _userManager = userManager;
-            _mapper = mapper;
+            _accountService = accountService;
         }
 
         [AllowAnonymous]
         [HttpPost("Login")]
         public async Task<IActionResult> Login([FromQuery] string username, [FromHeader(Name = "password")] string password)
         {
-            var user = await _userManager.FindByNameAsync(username);
+            try
+            {
+                await _accountService.Login(username, password);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
 
-            if (user is null)
-                throw new Exception("User does not exist.");
-
-            if (!await _userManager.CheckPasswordAsync(user, password))
-                throw new Exception("Incorrect password");
-
-            var result = await _signInManager.PasswordSignInAsync(user, password, false, false);
-
-            if(result.Succeeded)
-                return Ok();
-
-            return BadRequest(result.ToString());
+            return Ok();
         }
 
         [AllowAnonymous]
         [HttpPost("Register")]
         public async Task<IActionResult> Register([FromBody] UserPostModel userPostModel)
         {
-            var user = await _userManager.FindByNameAsync(userPostModel.UserName);
-            if (user != null)
-                throw new Exception("User with this username already exists");
+            try
+            {
+                await _accountService.Register(userPostModel);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
 
-            user = _mapper.Map<User>(userPostModel);
-            user.Id = Guid.NewGuid().ToString();
-            user.Balance = 0m;
-            user.EmailConfirmed = true;
-            var result = await _userManager.CreateAsync(user, userPostModel.Password);
-
-            if (!result.Succeeded)
-                return BadRequest(result.Errors);
-
-            return Ok();
+            return NoContent();
         }
     }
 }
