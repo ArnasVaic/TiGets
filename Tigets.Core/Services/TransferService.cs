a53846776617bbbb246cfe4f89afc8170a4638f1
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-10-02 21:29:24 +0300

    add transfer service

---
+++ Tigets.Core/Services/TransferService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tigets.Core.Models;
+using Tigets.Core.Repositories;
+
+namespace Tigets.Core.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly ITransferRepository _transferRepository;
+
+        public TransferService(ITransferRepository transferRepository)
+        {
+            _transferRepository = transferRepository;
+        }
+
+        public async Task CreateTransfer(string buyerId, Ticket ticket)
+        {
+            if (buyerId == ticket.UserId)
+                throw new Exception("Cannot transfer tickets to yourself");
+
+            var transfer = new Transfer
+            {
+                Id = Guid.NewGuid().ToString(),
+                Time = DateTime.UtcNow,
+                TicketId = ticket.Id,
+                ToId = buyerId,
+                FromId = ticket.UserId,
+                Cost = ticket.Cost
+            };
+
+            await _transferRepository.AddAsync(transfer);
+            await _transferRepository.SaveChangesAsync();
+        }
+    }
+}
