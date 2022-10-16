6debfa0baff9263f2e3ff30033058098a12ed4ea
Rugile Vasaityte <rugile.vasaityte@gmail.com>
2022-10-16 13:52:40 +0300

    .

--- frontend/src/pages/registerPage/RegisterPage.jsx
+++ frontend/src/pages/registerPage/RegisterPage.jsx
@@ -1,20 +1,24 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { postRegister } from "../../services/registerService";
-import { Typography, Button, Link } from "@mui/material";
+import { StyledCenteredColumn } from "../../generalComponents/styled/CenteredColumn.styled";
+import { StyledJustValueTextField } from "../../generalComponents/styled/JustValueTextField.styled";
+import { StyledTitle } from "../../generalComponents/styled/Title.styled";
+import { Typography, Button } from "@mui/material";
 import Alert from "@mui/material/Alert";
 import { LOGIN_URL } from '../../constants';
 import { getInfo } from "../../services/registerService";
 
+
 function RegisterPage() {
-  const [userName, setUsername] = useState();
-  const [password, setPassword] = useState();
-  const [cPassword, setCPassword] = useState();
-  const [name, setName] = useState();
-  const [surname, setSurname] = useState();
-  const [email, setEmail] = useState();
-  const [phoneNumber, setPhoneNumber] = useState();
+    const [userName, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const [cPassword, setCPassword] = useState();
+    const [name, setName] = useState();
+    const [surname, setSurname] = useState();
+    const [email, setEmail] = useState();
+    const [phoneNumber, setPhoneNumber] = useState();
 
     const [isError, setIsError] = useState(false);
     const [errorMsg, setErrorMsg] = useState();
@@ -22,62 +26,63 @@ function RegisterPage() {
     const [infoAlert, setInfoAlert] = useState(false);
     const [information, setInformation] = useState('');
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-  function checkValidation() {
-    if (password !== cPassword) {
-      setIsError(true);
-      setErrorMsg("Passwords do not match!");
-    } else {
-      setIsError(false);
+    function checkValidation() {
+        if (password !== cPassword) {
+            setIsError(true);
+            setErrorMsg("Passwords do not match!")
+        }
+        else {
+            setIsError(false);
+        }
     }
-  }
 
-  function handleData() {
-    const dataObject = {
-      userName,
-      password,
-      name,
-      surname,
-      email,
-      phoneNumber,
-    };
-    if (password === cPassword)
-      dispatch(
-        postRegister(dataObject, navigate, setErrorMsg, setIsError, setLoading)
-      );
-  }
+    function handleData() {
+        const dataObject = { userName, password, name, surname, email, phoneNumber };
+        if (password === cPassword) dispatch(postRegister(dataObject, navigate, setErrorMsg, setIsError, setLoading));
+    }
 
-  return (
-    <>
-      <StyledTitle>TIGETS</StyledTitle>
-      <StyledCenteredColumn>
-        <JustValueTextField label="userName" setValue={setUsername} />
-        <JustValueTextField
-          label="password"
-          type="password"
-          setValue={setPassword}
-        />
-        <JustValueTextField
-          label="confirm password"
-          type="password"
-          setValue={setCPassword}
-        />
-        <JustValueTextField label="name" setValue={setName} />
-        <JustValueTextField label="surname" setValue={setSurname} />
-        <JustValueTextField label="email" setValue={setEmail} />
-        <JustValueTextField label="phone number" setValue={setPhoneNumber} />
+    return (
+        <>
+            <StyledTitle>TIGETS</StyledTitle>
+            <StyledCenteredColumn>
+                <StyledJustValueTextField label="userName" setValue={setUsername} />
+                <StyledJustValueTextField
+                    label="password"
+                    type="password"
+                    setValue={setPassword}
+                />
+                <StyledJustValueTextField
+                    label="confirm password"
+                    type="password"
+                    setValue={setCPassword}
+                />
+                <StyledJustValueTextField
+                    label="name"
+                    setValue={setName}
+                />
+                <StyledJustValueTextField
+                    label="surname"
+                    setValue={setSurname}
+                />
+                <StyledJustValueTextField
+                    label="email"
+                    setValue={setEmail}
+                />
+                <StyledJustValueTextField
+                    label="phone number"
+                    setValue={setPhoneNumber}
+                />
 
-        <div>
-          {isError ? (
-            <Alert sx={{ mt: 1, marginBottom: 1 }} severity="error">
-              {errorMsg}
-            </Alert>
-          ) : (
-            ""
-          )}
-        </div>
+                <div>
+                    {isError ? (
+                        <Alert sx={{ mt: 1, marginBottom: 1 }} severity="error" >
+                            {errorMsg}
+                        </Alert>
+                    ) : ("")}
+                </div>
 
                 <Button
                     variant="contained"
@@ -90,7 +95,7 @@ function RegisterPage() {
                 >
                     Register
                 </Button>
-                {loading && <Typography style={{textAlign: "center"}}>Loading...</Typography>}
+                {loading && <Typography style={{ textAlign: "center" }}>Loading...</Typography>}
                 <Typography
                     style={{
                         textAlign: "center",
@@ -112,14 +117,14 @@ function RegisterPage() {
                     }}
                     onClick={() => {
                         dispatch(
-                           getInfo(setInfoAlert, setInformation));
+                            getInfo(setInfoAlert, setInformation));
                     }}
                 >
                     App information
                 </Button>
-           
+
                 {infoAlert && <Alert severity="info">{information}</Alert>}
-           
+
             </StyledCenteredColumn>
         </>
     );
