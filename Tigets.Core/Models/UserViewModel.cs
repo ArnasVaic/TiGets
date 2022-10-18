1f3f5a2b93ff3823f1002947564891a18545b85b
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-10-18 13:20:20 +0300

    readd model

---
+++ Tigets.Core/Models/UserViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Tigets.Core.Models
+{
+    public class UserViewModel
+    {
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
