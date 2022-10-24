ec562d0c1ec733a477717b9c583cccc279d4f18f
Naglis Šuliokas <naglis1134@gmail.com>
2022-10-24 22:18:55 +0300

    add date time picker (works?)

--- frontend/src/pages/profilePage/components/ImportTicketDialog.jsx
+++ frontend/src/pages/profilePage/components/ImportTicketDialog.jsx
@@ -9,13 +9,18 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import JustValueTextField from "../../../generalComponents/JustValueTextField";
 import { postImportTicket } from "../../../services/profileService";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import dayjs from "dayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { StyledJustValueTextField } from "../../../generalComponents/styled/JustValueTextField.styled";
 
 function ImportTicketDialog({ open, setOpen }) {
   const dispatch = useDispatch();
   const [eventName, setEventName] = useState();
   const [address, setAddress] = useState();
-  const [validFrom, setValidFrom] = useState();
-  const [validTo, setValidTo] = useState();
+  const [validFrom, setValidFrom] = useState(dayjs());
+  const [validTo, setValidTo] = useState(dayjs());
   const [cost, setCost] = useState();
 
   return (
@@ -28,8 +33,26 @@ function ImportTicketDialog({ open, setOpen }) {
       <DialogContent style={{ display: "flex", flexDirection: "column" }}>
         <JustValueTextField label="event name" setValue={setEventName} />
         <JustValueTextField label="address" setValue={setAddress} />
-        <JustValueTextField label="valid from" setValue={setValidFrom} />
-        <JustValueTextField label="valid to" setValue={setValidTo} />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DateTimePicker
+            label="valid from"
+            value={validFrom}
+            onChange={(value) => {
+              setValidFrom(value);
+            }}
+            renderInput={(params) => <StyledJustValueTextField {...params} />}
+          />
+          <DateTimePicker
+            label="valid to"
+            value={validTo}
+            onChange={(value) => {
+              setValidTo(value);
+              console.log(value);
+            }}
+            renderInput={(params) => <StyledJustValueTextField {...params} />}
+          />
+        </LocalizationProvider>
+
         <JustValueTextField label="cost (Eur)" setValue={setCost} />
       </DialogContent>
 
