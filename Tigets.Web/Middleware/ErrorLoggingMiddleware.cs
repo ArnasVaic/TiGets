121f74e4c2c25646685a2b6c54799617a9b3a5c3
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-10-24 23:06:41 +0300

    add error logging middleware

---
+++ Tigets.Web/Middleware/ErrorLoggingMiddleware.cs
@@ -0,0 +1,26 @@
+﻿namespace Tigets.Web.Middleware;
+
+public class ErrorLoggingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ErrorLoggingMiddleware> _logger;
+
+    public ErrorLoggingMiddleware(RequestDelegate next, ILogger<ErrorLoggingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            throw;
+        }
+    }
+}
