252509743c7b377a4b8d71d5830d285d6495c2a3
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-10-24 23:07:03 +0300

    configure serilog

--- Tigets.Web/Program.cs
+++ Tigets.Web/Program.cs
@@ -1,8 +1,13 @@
-using System.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Events;
 using Tigets.Web;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services)
+    .Enrich.FromLogContext()
+    .WriteTo.File("logs.txt", LogEventLevel.Warning));
 builder.Configuration.AddJsonFile("appsettings.json", false, true);
 var startup = new Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services);
