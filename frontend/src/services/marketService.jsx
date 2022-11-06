263363e4915dc221761ab1754ace2fb4ecdbfd8e
Naglis Šuliokas <naglis1134@gmail.com>
2022-11-06 04:18:41 +0200

    TG-112: ref buy alerts. all buy exceptions handeled

--- frontend/src/services/marketService.jsx
+++ frontend/src/services/marketService.jsx
@@ -1,30 +1,29 @@
 import { GET_MARKET_TICKETS, PATCH_BUY_URL } from "../constants";
 import { setMarketTickets } from "../slices/marketSlice";
 
-export const patchBuy = (ticketId, setErrorMsg) => async (dispatch) => {
-  try {
-    const response = await fetch(PATCH_BUY_URL(ticketId), {
-      method: "PATCH",
-      credentials: "include",
-    });
-    if (response.ok) {
-      alert("Ticket was successfully bought");
-      dispatch(getMarketTickets());
-    } else {
-        let errMsg = new TextDecoder().decode(
+export const patchBuy =
+  (ticketId, setErrorMsg, setSuccessMsg) => async (dispatch) => {
+    try {
+      const response = await fetch(PATCH_BUY_URL(ticketId), {
+        method: "PATCH",
+        credentials: "include",
+      });
+      if (response.ok) {
+        setSuccessMsg("Ticket was successfully bought");
+        setErrorMsg(null);
+        dispatch(getMarketTickets());
+      } else {
+        setErrorMsg(
+          new TextDecoder().decode(
             (await response.body.getReader().read()).value
+          )
         );
-        if (errMsg === "User does not have enough money to buy this ticket.") {
-            setErrorMsg("User does not have enough money to buy this ticket.");
-        }
-        else {
-            alert("Something went wrong. Please try again\n" + errMsg);
-        }
+        setSuccessMsg(null);
+      }
+    } catch (error) {
+      alert("Oops, server error");
     }
-  } catch (error) {
-    alert("Oops, server error");
-  }
-};
+  };
 
 export const getMarketTickets = () => async (dispatch) => {
   try {
@@ -37,7 +36,7 @@ export const getMarketTickets = () => async (dispatch) => {
     } else {
       let errMsg = new TextDecoder().decode(
         (await response.body.getReader().read()).value
-    );
+      );
       alert("Something went wrong. Please try again\n" + errMsg);
     }
   } catch (error) {
