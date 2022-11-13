456fdf47891beb38607a10fce4aba1d9427935c9
Arnas Vaicekauskas <32205690+ArnasVaic@users.noreply.github.com>
2022-11-13 15:50:12 +0200

    add interceptor (#44)

---
+++ Tigets.Core/Utilities/DbContextLoggingInterceptor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+using System.Data.Common;
+
+namespace Tigets.Core.Utilities
+{
+    public class DbContextLoggingInterceptor : DbCommandInterceptor
+    {
+        private readonly ILogger<DbContextLoggingInterceptor> _logger;
+
+        public DbContextLoggingInterceptor(ILogger<DbContextLoggingInterceptor> logger)
+        {
+            _logger = logger;
+        }
+
+        public override ValueTask<InterceptionResult<DbDataReader>> ReaderExecutingAsync(DbCommand command, CommandEventData eventData, InterceptionResult<DbDataReader> result,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            _logger.LogWarning($"{command.CommandText}");
+            return ValueTask.FromResult(result);
+        }
+
+        public override DbDataReader ReaderExecuted(DbCommand command, CommandExecutedEventData eventData, DbDataReader result)
+        {
+            _logger.LogWarning($"{command.CommandText}");
+            return result;
+        }
+    }
+}
