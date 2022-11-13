5411267cb8f9bf03eecd4576f109ca8fd891195a
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-11-13 15:58:11 +0200

    readd

---
+++ Tigets.Core/Services/Reading.cs
@@ -0,0 +1,29 @@
+﻿namespace Tigets.Core.Services
+{
+    public class Reading
+    {
+        public String AppInfo { get; }
+
+        private Reading()
+        {
+            AppInfo = readFromFile();
+        }
+
+        private string readFromFile()
+        {
+            string appInfo;
+            string path = Path.Combine(Directory.GetCurrentDirectory() + "\\Resources\\AppInformation.txt");
+            try
+            {
+                using StreamReader sr = new StreamReader(path);
+                appInfo = sr.ReadToEnd();
+            }
+            catch
+            {
+                appInfo = "The information about this app cannot be provided currently.";
+            }
+
+            return appInfo;
+        }
+    }
+}
